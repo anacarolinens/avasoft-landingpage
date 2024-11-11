4c67eb2ad53729988edfad5c8edbf059e5d89906
Ana Caroline <85416744+anacarolinens@users.noreply.github.com>
2024-11-11 00:19:04 -0500

    :sparkles: feat: update PasswordResetConfirm

--- src/components/PasswordResetConfirm.vue
+++ src/components/PasswordResetConfirm.vue
@@ -1,24 +1,43 @@
 <template>
-    <div class="container-reset flex justify-center items-center max-w-full pt-1">
-        <div class="flex flex-col items-center">
-            <img src="../assets/logo-avasoft.png" alt="logo avasoft" class="w-25 h-20" />
-            <h1 class="mt-10 mb-10 rounded font-semibold">REDEFINIÇÃO DE SENHA</h1>
+    <div class="container-reset flex flex-col items-center max-w-full pt-1">
+        
+      <!-- Logo da Avasoft -->
+      <img src="../assets/logo-avasoft.png" alt="logo avasoft" class="w-25 h-20" />
+  
+      <!-- Título de redefinição de senha -->
+      <h1 class="mt-4 mb-10 text-xl font-semibold">REDEFINIÇÃO DE SENHA</h1>
+  
+      <!-- Formulário de redefinição de senha -->
+      <form action="" class="w-full max-w-md">
+        <div class="flex flex-col">
+          <label for="newPassword">Nova Senha</label>
+          <input
+            type="password"
+            placeholder="Digite sua senha aqui..."
+            class="mt-2 mb-5 rounded border border-stone-700 p-2"
+            v-model="newPassword"
+          />
+  
+          <label for="confirmPassword">Confirmar Senha</label>
+          <input
+            type="password"
+            placeholder="Digite a senha novamente..."
+            class="mt-2 mb-5 rounded border border-stone-700 p-2"
+            v-model="confirmPassword"
+          />
         </div>
-
-        <form action="" class="w-full max-w-md" >
-            <div class="flex flex-col">
-                <label for="newPassword">Nova Senha</label>
-                <input type="password" placeholder="Digite sua senha aqui..." class="mt-2 mb-5 rounded border border-stone-700 p-2" v-model="newPassword">
-                
-                <label for="confirmPassword">Confirmar Senha</label>
-                <input type="password" placeholder="Digite a senha novamente..." class="mt-2 mb-5 rounded border border-stone-700 p-2" v-model="confirmPassword">
-            </div>
-
-            <div class="flex flex-col items-center justify-center gap-3">
-                <button type="button" class="flex items-center bg-[#FF8139]  px-12 py-3 rounded-md text-white hover:bg-[#FF5C00]" @click="handlePasswordReset">Redefinir</button>
-                <a><router-link to="/" class=" text-black"> Fazer Login </router-link></a>
-            </div>
-        </form>
+  
+        <div class="flex flex-col items-center justify-center gap-3">
+          <button
+            type="button"
+            class="flex items-center bg-[#FF8139] px-12 py-3 rounded-md text-white hover:bg-[#FF5C00]"
+            @click="handlePasswordReset"
+          >
+            Redefinir
+          </button>
+          <router-link to="/" class="text-black cursor-pointer underline">Fazer Login</router-link>
+        </div>
+      </form>
     </div>
 </template>
 
